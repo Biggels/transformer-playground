46589f86b6eee014d5deb72c5a704b9dd62db877
Ray <haackra@gmail.com>
2026-02-14 21:10:48 -0500

    Reduce model size and training steps in default config

--- configs/default.py
+++ configs/default.py
@@ -7,23 +7,25 @@ def get_config() -> ExperimentConfig:
     cfg.data.val_fraction = 0.1
 
     cfg.tokenizer.kind = "sentencepiece_bpe"
-    cfg.tokenizer.vocab_size = 3200
+    cfg.tokenizer.vocab_size = 1600
 
-    cfg.model.d_model = 384
-    cfg.model.n_layers = 12
-    cfg.model.n_heads = 12
+    cfg.model.d_model = 256
+    cfg.model.n_layers = 6
+    cfg.model.n_heads = 8
     cfg.model.context_length = 64
+    cfg.model.dropout = 0.2
     cfg.model.mlp_enabled = True
     cfg.model.positional_encoding = "learned"
     cfg.model.activation = "gelu"
 
     cfg.train.batch_size = 64
     cfg.train.grad_accum_steps = 2
-    cfg.train.max_steps = 10000
+    cfg.train.max_steps = 4000
     cfg.train.eval_interval = 100
     cfg.train.sample_interval = 100
     cfg.train.log_interval = 20
-    cfg.train.eval_batches = 16
+    cfg.train.eval_batches = 64
+    cfg.train.weight_decay = 0.2
 
     cfg.sampling.max_new_tokens = 24
     cfg.sampling.temperature = 0.75
